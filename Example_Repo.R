faa79f2c5153272c6db7f484715811ad505ce5ba
Lizbeth Garcia <lgarcia46@gsu.edu>
2026-02-20 09:47:59 -0500

    color-change

--- Example_Repo.R
+++ Example_Repo.R
@@ -4,4 +4,4 @@ print("This is a sample script!")
 # Simple scatter plot
 x <- c(1, 2, 3, 4, 5)
 y <- c(2, 4, 6, 8, 10)
-plot(x, y, main="Scatter Plot", xlab="X-axis", ylab="Y-axis", col="purple", pch=19)
+plot(x, y, main="Scatter Plot", xlab="X-axis", ylab="Y-axis", col="orange", pch=19)
