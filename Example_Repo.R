332ab344a2973ec7e1b6160104ddcbbbcbdcf80b
Lizbeth Garcia <lgarcia46@gsu.edu>
2026-02-20 08:21:50 -0500

    Commit file

---
+++ Example_Repo.R
@@ -0,0 +1,7 @@
+print("This is a sample script!")
+
+#This is a sample scatter plot
+# Simple scatter plot
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 6, 8, 10)
+plot(x, y, main="Scatter Plot", xlab="X-axis", ylab="Y-axis", col="blue", pch=19)
